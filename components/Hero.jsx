48cbab101f3a0f7576fff0ba7b52e8de412a3f28
fantaz-st <cbabic@pfst.hr>
2023-05-13 14:02:40 +0200

    Page init animations

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -15,14 +15,79 @@ gsap.registerPlugin(ScrollToPlugin);
 const Hero = () => {
   const comp = useRef();
   const scrollContainerRef = useRef(null);
+  const overlayRef = useRef(null);
+  const row1Ref = useRef(null);
+  const row2Ref = useRef(null);
 
   useIsomorphicLayoutEffect(() => {
     let ctx = gsap.context(() => {
+      const slogans = gsap.utils.toArray(".slogan");
+      const tl = gsap.timeline();
+      const duration = 0.5;
+      const tl2 = gsap.timeline({ paused: true });
+      slogans.forEach((slogan, i) => {
+        tl2.to(slogans.slice(0, i), {
+          y: `-=80`,
+          duration: duration,
+        });
+
+        tl.from(slogan, {
+          y: 80,
+          duration: duration,
+          autoAlpha: true,
+          ease: "easeInOut",
+          onStart: () => {
+            tl2.play();
+          },
+        });
+      });
+      tl.to(overlayRef.current, {
+        opacity: 0,
+        duration: 0.6,
+        ease: "easeInOut",
+        display: "none",
+      });
+      tl.from(row1Ref.current, {
+        y: -100,
+        opacity: 0,
+        ease: "easeOut",
+        delay: -0.3,
+      });
+      tl.from(row2Ref.current, {
+        y: -100,
+        opacity: 0,
+        ease: "easeOut",
+        delay: -0.4,
+      });
+
+      /* slogans.forEach((slogan, i) => {
+        const tl2 = gsap.timeline({ paused: true });
+
+        tl2.to(slogans.slice(0, i), {
+          y: `-=50`,
+          duration: 0.55,
+        });
+
+        tl.from(slogan, {
+          autoAlpha: true,
+          y: 50,
+          duration: 0.55,
+          ease: "none",
+          onStart: () => {
+            tl2.play();
+          },
+        });
+      });
+ */
+      // -------------------------------------------------------------------------------
+
       let restartTimer;
+
       let t = gsap.to(scrollContainerRef.current, {
         scrollTo: { y: "max", autoKill: true },
         duration: 600,
       });
+
       Observer.create({
         target: scrollContainerRef.current,
         type: "wheel,touch",
@@ -44,19 +109,17 @@ const Hero = () => {
 
   return (
     <section className='w-full flex flex-row-reverse gap-8' ref={comp}>
+      <div className='absolute top-0 left-0 w-full h-screen bg-blue-50 block z-30' ref={overlayRef}></div>
       <div className='w-full h-screen flex content-center items-center bg-blue-50'>
-        <div className='w-full flex flex-col justify-between pt-[24.5vh] pr-[5vw] pb-[3.75rem] pl-[4vw]'>
-          <h1 className='text-7xl uppercase'>
-            Navigating
-            <br />
-            the future
-            <br />
-            of maritime
-            <br />
-            industry
-          </h1>
+        <div className='w-full flex flex-col justify-between mx-[5vw] relative'>
+          <div className='relative h-[50vh] overflow-hidden z-40'>
+            <h1 className='text-8xl uppercase slogan absolute bottom-20'>Navigating</h1>
+            <h1 className='text-8xl uppercase slogan absolute bottom-20'>the future</h1>
+            <h1 className='text-8xl uppercase slogan absolute bottom-20'>of maritime</h1>
+            <h1 className='text-8xl uppercase slogan absolute bottom-20'>industry</h1>
+          </div>
           <div className='ml-auto'>
-            <p className='text-3xl mb-8'>
+            <p className='text-2xl mb-8'>
               Over 60 years of <br />
               tradition in training, <br />
               teaching and knowledge <br />
@@ -68,11 +131,11 @@ const Hero = () => {
       </div>
       <div className='w-full h-screen z-0'>
         <div className='flex flex-row gap-8 relative overflow-x-scroll scrollbar-hide' ref={scrollContainerRef} id='row-scroller'>
-          <div className='flex flex-col gap-8 h-screen w-full'>
+          <div className='flex flex-col gap-8 h-screen w-full' ref={row1Ref}>
             {images.row1.map((image) => (
               <div key={image.id} className={`${image.light && "light-image"} relative`}>
                 <div className='aspect-[3/4] relative h-full w-full'>
-                  <Image src={image.image} alt={image.description} fill className='object-cover' sizes='25vw' />
+                  <Image src={image.image} alt={image.description} fill className='object-cover' sizes='50vw' />
                 </div>
                 <p className={`uppercase font-bold absolute bottom-0 px-6 pb-6 ${image.light ? "text-black" : "text-white"}`}>
                   {image.title} • <span className='normal-case font-normal'>{image.description}</span>
@@ -80,11 +143,11 @@ const Hero = () => {
               </div>
             ))}
           </div>
-          <div className='flex flex-col gap-8 h-screen w-full'>
+          <div className='flex flex-col gap-8 h-screen w-full' ref={row2Ref}>
             {images.row2.map((image) => (
               <div key={image.id} className='relative'>
                 <div className='aspect-[2/3] relative h-full w-full'>
-                  <Image src={image.image} alt={image.description} fill className='object-cover' sizes='25vw' />
+                  <Image src={image.image} alt={image.description} fill className='object-cover' sizes='50vw' />
                 </div>
                 <p className={`uppercase font-bold absolute bottom-0 px-6 pb-6 ${image.light ? "text-black" : "text-white"}`}>
                   {image.title} • <span className='normal-case font-normal'>{image.description}</span>
